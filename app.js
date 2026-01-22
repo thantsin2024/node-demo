90f501099362efe9836ab393f26ad1553fca1b58
Peter <thant@3sam.io>
2026-01-22 16:02:15 +0700

    remove unused endpoint

--- app.js
+++ app.js
@@ -5,11 +5,6 @@ app.get('/', (req, res) => {
   res.send('🚀 Node.js App Deployed via Jenkins + Docker!');
 });
 
-
-app.get('/test', (req, res) => {
-    res.send('hello world');
-  });
-
 const PORT = 3000;
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}`);
